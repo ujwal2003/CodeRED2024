c9e8044f8ef9bc58e44c2df50a164911343025ad
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-10 17:20:11 -0600

    completed extraction code in route

--- backend/routes/data-extraction-route.js
+++ backend/routes/data-extraction-route.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/get', (req, res) => {
+const nlp = require('compromise');
+nlp.extend(require('compromise-dates'));
+const stringSimilarity = require('string-similarity');
 
+function closestSubsetStr(target, options) {
+    let closestOption = null;
+    let highestSimilarity = -1;
+    
+    options.forEach(option => {
+        const similarity = stringSimilarity.compareTwoStrings(target, option);
+        if (similarity > highestSimilarity) {
+            closestOption = option;
+            highestSimilarity = similarity;
+        }
+    });
+
+    return closestOption;
+}
+
+router.post('/get', (req, res) => {
+    let { userPrompt } = req.body;
+    const doc = nlp(userPrompt.toLowerCase());
+
+    const startLocationContext = doc.match('(from|between) #Place').out('normal');
+    const destinationContext = doc.match('(to|and) #Place').out('normal');
+    const locations = doc.places().out('array');
+
+    const startingLocation = closestSubsetStr(startLocationContext, locations);
+    const destination = closestSubsetStr(destinationContext, locations);
+
+    const dates = doc.dates().get();
+
+    const response = {
+        "locations": {
+            "start": startingLocation,
+            "dest": destination,
+            "mentioned_places": locations
+        },
+        "datetimes": { dates }
+    };
+
+    res.status(200).json(response);
 });
 
 module.exports = router;
