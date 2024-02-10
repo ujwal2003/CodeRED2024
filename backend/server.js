63e4f0d74318925502f276a7c3cc7fb66c571592
ujwal2003 <joshiujwal2003@gmail.com>
2024-02-10 17:30:03 -0600

    implemented data extraction route

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("Server is online.");
 });
+
+// routes
+const userPromptRouter = require('./routes/data-extraction-route');
+app.use("/prompt", userPromptRouter);
 
 app.listen(port, () => {
     console.log(`Server running on prt ${port}`);
