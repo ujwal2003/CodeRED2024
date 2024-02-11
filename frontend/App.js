00805325e272740d9a81c6a0f12a96aeb322a5d7
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-02-10 21:30:17 -0600

    Made separate component.js for modularity

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View, SafeAreaView, Text, StyleSheet, Image, ScrollView, TextInput } from 'react-native';
+import GradientBackground from './GradientBackground';
 import { LinearGradient } from 'expo-linear-gradient';
 
 // View --> UIView
 // SafeAreaView --> Helps notches not cover content
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        colors={['#115E81', '#86BBD8']}
-        style={{ flex: 1, width: '100%' }}
-      >
-        <SafeAreaView>
+    <View style={{ flex: 1 }}>
+      <GradientBackground>
           <Text style={styles.text}>Open up App.js to start working on your app!</Text>
           <StatusBar style="auto" />
-        </SafeAreaView>
-      </LinearGradient>
+      </GradientBackground>
     </View>
   );
 }
