fe0509ed31b620a8d2968c85440951a4e494ec78
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-02-11 02:55:23 -0600

    WelcomeScreen InfoBox Addition

--- frontend/WelcomeScreen.js
+++ frontend/WelcomeScreen.js
@@ -4,51 +4,65 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import GradientBackground from './GradientBackground';
 
 const WelcomeScreen = ({ navigation }) => {
-  const handlePress = () => {
-    navigation.navigate('User');
-  };
+    const handlePress = () => {
+        navigation.navigate('User');
+    };
 
-  return (
-    <GradientBackground>
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.touchable} onPress={handlePress}>
-          <Text style={styles.text}>Get Started</Text>
-        </TouchableOpacity>
-        <View styles={styles.bottomBox}>
-            <Text style={styles.bottomText}>Uncover budget-friendly flights at the simplest request.</Text>
-        </View>
-      </View>
-    </GradientBackground>
-  );
+    return (
+        <GradientBackground>
+            <View style={styles.container}>
+                <View style={styles.infoBox}>
+                    <Text style={styles.infoText}>
+                        Uncover <Text style={styles.boldText}>budget-friendly</Text> flights at the 
+                        <Text style={styles.boldText}> simplest</Text> request.
+                    </Text>
+                </View>
+                <TouchableOpacity style={styles.touchable} onPress={handlePress}>
+                    <Text style={styles.text}>Get Started</Text>
+                </TouchableOpacity>
+            </View>
+        </GradientBackground>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  touchable: {
-    padding: 20,
-    backgroundColor: '#90BE6D',
-    borderRadius: 10,
-    width: '45%',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    touchable: {
+        padding: 20,
+        bottom: -90,
+        backgroundColor: '#90BE6D',
+        borderRadius: 10,
+        width: '45%',
 
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    fontSize: 20,
-  },
-  bottomBox: {
-    marginTop: 20, // Adjust spacing as needed
-    padding: 20,
-    backgroundColor: '#F4F5E6', // Example color
-    borderRadius: 10,
-  },
-  bottomText: {
-    fontSize: 15
-  }
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 20,
+    },
+    infoBox: {
+        position: 'absolute',
+        bottom: 250,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F4F5E6',
+        borderRadius: 10,
+        paddingHorizontal: 50,
+        paddingTop: 250,
+        paddingBottom: 125,
+    },
+    infoText: {
+        fontSize: 15,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    boldText: {
+        fontWeight: 'bold',
+      },
 });
 
 export default WelcomeScreen;
