82c667519f2e57f1d69fd57a1b8a053553504f5b
aaurtaza <alvaroaurtaza@gmail.com>
2024-02-10 19:58:11 -0600

    added amadeus api

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {
 // routes
 const userPromptRouter = require('./routes/data-extraction-route');
 app.use("/prompt", userPromptRouter);
+const comparePriceRouter = require("./routes/compare-prices")
+app.use("/prices", comparePriceRouter)
 
 app.listen(port, () => {
     console.log(`Server running on prt ${port}`);
