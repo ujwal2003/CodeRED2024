b62c833ec9b11ae1bbdd4f5e55bff47f6cb8c135
Anthony Delgado <115326818+AnthonyD4101@users.noreply.github.com>
2024-02-11 04:56:44 -0600

    added cors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+let cors = require('cors');
+app.use(cors());
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
